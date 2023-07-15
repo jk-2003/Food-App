a5abe146e38a0902866c45a5a2945a4cdcf6c7ec
jk <jcghchgc@.com>
2023-07-15 23:02:30 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Cart from "./components/Cart/Cart";
 import HeadContentBox from "./components/headcontentbox/headcontentbox";
@@ -6,25 +6,26 @@ import Header from "./components/header/Header";
 import Mealslist from "./components/meals/mealslist";
 import CartProvidedContext from "./components/contex/CartProvidedContex";
 
-
-
 function App() {
-  const [open,setopen]=useState(false)
+  const [open, setopen] = useState(false);
 
-  function openCart(){
-    setopen(true)
+  function openCart() {
+    setopen(true);
   }
 
-  function closeCart(){
-    setopen(false)
+  function closeCart() {
+    setopen(false);
+  }
+  function newapp() {
+    console.log("hello");
   }
   return (
     <div className="App">
-      < CartProvidedContext>
-      <Header openCart={openCart} />
-      <HeadContentBox />
-      <Mealslist />
-      <Cart closeCart={closeCart} opencart={open}/>
+      <CartProvidedContext>
+        <Header openCart={openCart} />
+        <HeadContentBox />
+        <Mealslist />
+        <Cart closeCart={closeCart} opencart={open} />
       </CartProvidedContext>
     </div>
   );
